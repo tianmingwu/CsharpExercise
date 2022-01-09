b6ae2130af7ee95ada6fdbd46ecf5754a5effd41
tianmingwu <tianming.y.wu@outlook.com>
2022-01-09 16:50:01 -0600

    to exercise 16

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Exercise 1:");
+﻿/*
+Console.WriteLine("Exercise 1:");
 LinqExercise.Exercise1.Solve();
 Console.WriteLine("\n");
 
@@ -15,6 +16,34 @@ Console.WriteLine("\n");
 Console.WriteLine("Exercise 4:");
 LinqExercise.Exercise4.Solve();
 Console.WriteLine("\n");
+
+Console.WriteLine("Exercise 5:");
+LinqExercise.Exercise5.Solve();
+Console.WriteLine("\n");
+
+Console.WriteLine("Exercise 6:");
+LinqExercise.Exercise6.Solve();
+Console.WriteLine("\n");
+
+Console.WriteLine("Exercise 7:");
+LinqExercise.Exercise7.Solve();
+Console.WriteLine("\n");
+
+Console.WriteLine("Exercise 8:");
+LinqExercise.Exercise8.Solve();
+Console.WriteLine("\n");
+
+Console.WriteLine("Exercise 11:");
+LinqExercise.Exercise11.Solve();
+Console.WriteLine("\n");
+
+Console.WriteLine("Exercise 13:");
+LinqExercise.Exercise13.Solve();
+Console.WriteLine("\n");
+
+*/
+Console.WriteLine("Exercise 16:");
+LinqExercise.Exercise16.Solve();
 
 namespace LinqExercise
 {
@@ -86,5 +115,109 @@ namespace LinqExercise
             }
         }
     }
+    public class Exercise5
+    {   
+        private static string? InputString;
+        public static void Solve()
+        {
+            Console.Write("Input the string: ");
+            InputString = Console.ReadLine() ?? "";
 
+            foreach(var obj in InputString.ToArray().GroupBy(x=>x))
+            {
+                int count = obj.Count();
+                Console.WriteLine($"Character {obj.Key}: {count} {(count==1?"time":"times")}");
+            }
+        }
+    }
+    public class Exercise6
+    {
+        public static void Solve()
+        {
+            foreach(var day in Enum.GetNames(typeof(DayOfWeek)))
+            {
+                Console.WriteLine(day);
+            }
+        }
+    }
+    public class Exercise7
+    {
+        private static int[] Numbers = {5, 1, 9, 2, 3, 7, 4, 5, 6, 8, 7, 6, 3, 4, 5, 2};
+        public static void Solve()
+        {
+            foreach(var obj in Numbers.GroupBy(x=>x))
+            {
+                int num = obj.Key;
+                int fre = obj.Count();
+                Console.WriteLine($"{num} {num*fre} {fre}");
+            }
+        }
+    }
+    public class Exercise8
+    {
+        static string[] Cities = {"ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "new delhi",
+        "AMSTERDAM", "ABU DHABI", "PARIS", "PALM SPRINGS"};
+        public static void Solve()
+        {
+            Console.Write("Input starting character for the string:");
+            string startingChar = (Console.ReadLine() ?? "*").ToUpper();
+
+            Console.Write("Input ending character for the string:");
+            string endingChar = (Console.ReadLine() ?? "*").ToUpper();
+
+            foreach(var city in Cities.Where(x=>(x.ToUpper().StartsWith(startingChar) && x.ToUpper().EndsWith(endingChar))))
+            {
+                Console.WriteLine($"The city starting with {startingChar} and ending with {endingChar} is: {city.ToUpper()}");
+            }
+        }
+    }
+    public class Exercise11
+    {
+        private static int[] Members = {5, 7, 13, 24, 6, 9, 8, 7, 11, 101};
+        public static void Solve()
+        {
+            Console.Write($"Please enter an integer in range [0, {Members.Length+1}):");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"The top {n} records from the list are:");
+            foreach(var member in Members.OrderByDescending(x=>x).Take(n))
+            {
+                Console.WriteLine(member);
+            }
+        }
+    }
+    public class Exercise13
+    {
+        public static void Solve()
+        {
+            Console.Write("Input number of strings to store in the array: ");
+            int numberElements = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Input {numberElements} strings for the array:");
+
+            string?[] stringArray = new string[numberElements];
+            for (int i=0;i<numberElements;i++)
+            {
+                Console.Write($"Element[{i}]: ");
+                stringArray[i] = Console.ReadLine();
+            }
+            string newString = String.Join(", ", stringArray);
+            Console.WriteLine($"The new string is {newString}");
+        }
+    }
+    public class Exercise16
+    {
+        public static void Solve()
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            
+            var fileInfos = Directory.EnumerateFiles(currentDirectory)
+            .Select(file=>new FileInfo(file));
+
+            foreach(var obj in fileInfos)
+            {
+                Console.WriteLine($"{obj.Name}: {obj.Length}");
+            }
+            Console.WriteLine($"The average size of the files in this directory is {fileInfos.Select(x=>x.Length).Average()}.");
+        }
+    }
 }
